dcd4ce11a126da19507be8323536e146205862ad
Dave Houston <d.houston4@students.clark.edu>
2021-02-06 12:05:27 -0800

    Update with POC for sensehat
    Updated gitignore to not commit certificates

---
+++ client/iot.py
@@ -0,0 +1,36 @@
+from awscrt import io, mqtt, auth, http
+from awsiot import mqtt_connection_builder
+import time as t
+import json
+
+ENDPOINT = "a1qecpjelyfwp0-ats.iot.us-east-1.amazonaws.com"
+CLIENT_ID = "ntecpi"
+PATH_TO_CERT = "cert/7752c08c83-certificate.pem.crt"
+PATH_TO_KEY = "cert/7752c08c83-private.pem.key"
+PATH_TO_ROOT = "cert/AmazonRootCA1.pem"
+
+MESSAGE = "Hello, world!"
+TOPIC = "test/testing"
+RANGE = 20
+
+event_loop_group = io.EventLoopGroup(1)
+host_resolver = io.DefaultHostResolver(event_loop_group)
+client_bootstrap = io.ClientBootstrap(event_loop_group, host_resolver)
+mqtt_connection = mqtt_connection_builder.mtls_from_path( endpoint=ENDPOINT, cert_filepath=PATH_TO_CERT, pri_key_filepath=PATH_TO_KEY, client_bootstrap=client_bootstrap, ca_filepath=PATH_TO_ROOT, client_id=CLIENT_ID, clean_session=False, keep_alive_secs=6)
+print("Connecting to {} with client ID '{}'...".format(ENDPOINT, CLIENT_ID))
+# Make the connect() call
+connect_future = mqtt_connection.connect()
+# Future.result() waits until a result is available
+connect_future.result()
+print("Connected!")
+# Publish message to server desired number of times.
+print('Begin Publish')
+for i in range (RANGE):
+    data = "{} [{}]".format(MESSAGE, i+1)
+    message = {"message" : data}
+    mqtt_connection.publish(topic=TOPIC, payload=json.dumps(message), qos=mqtt.QoS.AT_LEAST_ONCE)
+    print("Published: '" + json.dumps(message) + "' to the topic: " + "'test/testing'")
+    t.sleep(0.1)
+print('Publish End')
+disconnect_future = mqtt_connection.disconnect()
+disconnect_future.result()
--- client/main.py
+++ client/main.py
@@ -1,20 +1,71 @@
-import serial, time, json
+import serial
+import time
+import json
+from sense_hat import SenseHat
 
-airSerial = serial.Serial('/dev/ttyUSB0')
+senseHat = None
+airSerial = None
 
-while True:
+# Initial sensor setup, always happens.
+try:
+    senseHat = SenseHat()
+except Exception:
+    print("Problem loading SenseHat, aborting.")
+    exit
+
+try:
+    airSerial = serial.Serial('/dev/ttyUSB0')
+except Exception:
+    print("Problem connecting to AQ sensor, aborting!")
+    exit
+
+
+def getAQ(airSerial):
     data = []
-    for index in range(0,10):
+    for index in range(0, 10):
         serIn = airSerial.read()
         data.append(serIn)
 
     pmTwoFive = int.from_bytes(b''.join(data[2:4]), byteorder='little') / 10
     pmTen = int.from_bytes(b''.join(data[4:6]), byteorder='little') / 10
 
-    output = { 'source': 'particulate', 'pm25': pmTwoFive, 'pm10': pmTen }
-    print(json.dumps(output))
-    time.sleep(30)
+    return [pmTwoFive, pmTen]
 
 
+def getTemp(senseHat):
+    temp = senseHat.temperature
+    humid = senseHat.get_temperature_from_humidity()
+    pressure = senseHat.get_temperature_from_pressure()
+    return ((temp + humid + pressure) / 3)
 
 
+def getPressure(senseHat):
+    return senseHat.get_pressure()
+
+
+def getHumidity(senseHat):
+    return senseHat.get_humidity()
+
+
+def sendIt(data):
+    print(json.dumps(data))
+
+
+def main():
+    while True:
+        AQ = getAQ(airSerial)
+        temp = getTemp(senseHat)
+        pressure = getPressure(senseHat)
+        humidity = getHumidity(senseHat)
+        output = {
+                'pm25': AQ[0],
+                'pm10': AQ[1],
+                'tempC': temp,
+                'pressureMb': pressure,
+                'humidityPct': humidity
+                }
+        sendIt(output)
+        time.sleep(10)
+
+
+main()
